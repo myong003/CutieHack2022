8f4e762a184fda3b6dbf68a4681db544b0b59db3
myong003 <matthew.yong0801@gmail.com>
2022-11-05 16:43:18 -0700

    Added cloud animations

--- Assets/Scripts/MattWeather/CloudSpawner.cs
+++ Assets/Scripts/MattWeather/CloudSpawner.cs
@@ -30,16 +30,18 @@ public class CloudSpawner : MonoBehaviour
 
     public void SpawnCloud() {
         float thunderRandom = Random.Range(0, 5);
+        Vector3 randomOffset = new Vector3(0, Random.Range(-0.5f, 0.5f), 0);
+
         Vector3 currPos = this.transform.position;
         if ( thunderRandom == 1 )
         {
-            ThunderScript tempthunderCloud = Instantiate(thunderCloud, currPos + new Vector3(0, Random.Range(-1, 1)), Quaternion.identity).GetComponent<ThunderScript>();
+            ThunderScript tempthunderCloud = Instantiate(thunderCloud, currPos + randomOffset, Quaternion.identity).GetComponent<ThunderScript>();
             tempthunderCloud.direction = this.direction;
             Destroy(tempthunderCloud.gameObject, 10f);
         }
         else 
         {
-            CloudScript tempCloud = Instantiate(cloud, currPos + new Vector3(0, Random.Range(-1, 1)), Quaternion.identity).GetComponent<CloudScript>();
+            CloudScript tempCloud = Instantiate(cloud, currPos + randomOffset, Quaternion.identity).GetComponent<CloudScript>();
             tempCloud.direction = this.direction;
             Destroy(tempCloud.gameObject, 10f);
         }
