2f2bdf087c908eff7513ea5639885e612800b743
Garrick Chiu <74120169+GarrickC23@users.noreply.github.com>
2022-11-05 16:55:47 -0700

    LevelSelection

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -39,5 +41,6 @@ public class GameManager : MonoBehaviour
 
     public void EndLevel() {
         Debug.Log("Day ended");
+        SceneManager.LoadScene("LevelSelection");
     }
 }
