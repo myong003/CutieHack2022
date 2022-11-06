fa1e5bfe06c2a348de11f29d6402e79810ac4406
brentnicks <brent_nicks@msn.com>
2022-11-05 18:26:48 -0700

    Updated Battery

--- Assets/Scripts/Managers/BatteryManager.cs
+++ Assets/Scripts/Managers/BatteryManager.cs
@@ -19,6 +19,8 @@ public class BatteryManager : MonoBehaviour
     private float batteryPercent;
     private float batteryIncrease;
 
+    public float batteryMultiplier = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +31,7 @@ public class BatteryManager : MonoBehaviour
         // Since CheckPanels is called every frame, for endTime seconds, each increase must add up to max battery
         // Divide by 3 for each solar panel
         float totalSamples = GameManager.Instance.endTime * 60;
-        batteryIncrease = Mathf.Abs(150 / totalSamples / solarPanels.Count);
+        batteryIncrease = Mathf.Abs(150 / totalSamples / solarPanels.Count) * batteryMultiplier;
     }
 
     // Update is called once per frame
