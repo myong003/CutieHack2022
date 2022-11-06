68d9ac8f5a35911ef20a13321abe9659fcabb69a
myong003 <matthew.yong0801@gmail.com>
2022-11-05 20:12:16 -0700

    Added fade to black

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,6 +8,12 @@ using UnityEngine.Tilemaps;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance {get; private set; }
+    public GameObject endScreenLose;
+    public GameObject endScreenWin;
+    public Image blackScreenLose;
+    public Image blackScreenWin;
+    public float fadeSpeed;
+
     public BatteryManager batteryManager;
     public float endTime;
     GameObject Sky;
@@ -15,6 +21,7 @@ public class GameManager : MonoBehaviour
     GameObject FogTileMap;
 
     private float currTime, skyRotateAngle, FogTileMapA;
+    private bool gameEnded = false;
 
     void Awake() {
         if (Instance != null && Instance != this) {
@@ -44,20 +51,47 @@ public class GameManager : MonoBehaviour
         Sky.transform.RotateAround(Sky.transform.position, Vector3.forward, -skyRotateAngle);
         FogTileMapA += (140 / endTime) / 60;
         FogTileMap.GetComponent<Tilemap>().color = new Color(0.4716981f,0.4716981f,0.4716981f, FogTileMapA/255); //gray with increasing opacity
-        if (currTime >= endTime) {
+        if (currTime >= endTime && !gameEnded) {
             EndLevel();
         }
     }
 
     public void EndLevel() {
         Debug.Log("Day ended");
-        if ( batteryManager.batteryPercent < 100 )
+        if ( batteryManager.batteryPercent < 100 && !gameEnded)
         {
-            SceneManager.LoadScene("GameOverScreen");
+            // SceneManager.LoadScene("GameOverScreen");
+            StartCoroutine("FadeToBlackLose");
         }
         else
         {
-            SceneManager.LoadScene("WinScreen"); 
+            // SceneManager.LoadScene("WinScreen"); 
+            StartCoroutine("FadeToBlackWin");
         }
+    }
+
+
+    IEnumerator FadeToBlackLose() {
+        gameEnded = true;
+        while (blackScreenLose.color.a < 1f) {
+            Color c = blackScreenLose.color;
+            c.a += fadeSpeed;
+            blackScreenLose.color = c;
+            yield return null;
+        }
+
+        endScreenLose.SetActive(true);
+    }
+    
+    IEnumerator FadeToBlackWin() {
+        gameEnded = true;
+        while (blackScreenWin.color.a < 1f) {
+            Color c = blackScreenWin.color;
+            c.a += fadeSpeed;
+            blackScreenWin.color = c;
+            yield return null;
+        }
+
+        endScreenWin.SetActive(true);
     }
 }
